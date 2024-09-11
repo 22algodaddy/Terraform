57e0c520562621791490ee75615a4f9f87930484
Dhruv Choubey <Choubeydhruv2014@gmail.com>
2024-09-12 00:02:17 +0530

    Craeted main.tf file

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.2.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+}
+
+# Create a VPC network
+resource "google_compute_network" "peering_network" {
+  name = "peering-network"
+
+}
